dbdf6c6790fb6b2db40ee4a6ac17d115eceae046
EC2 Default User <ec2-user@ip-172-31-10-155.us-east-2.compute.internal>
2019-02-02 17:46:58 +0000

    submission

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -2,34 +2,67 @@
 
 # Part 1
 
-def sum arr
-  # YOUR CODE HERE
+def sum(arr)
+  s = 0
+	arr.each {|x| s+= x}
+	s
 end
-
-def max_2_sum arr
-  # YOUR CODE HERE
+#====================
+def max_2_sum(arr)
+  return 0 if arr.empty?
+	return arr[0] if arr.length == 1
+	ar = arr.sort
+	ar[-1] + ar[-2]
 end
-
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+#====================
+def sum_to_n?(arr, n)
+  return false if arr.empty? or arr.length == 1
+  
+  h = Hash.new
+	arr.each{|x| 
+		if h.key? x
+			return true
+		else
+			h[n - x] = n	
+		end
+	}
+	return false
 end
-
+#================
+#================
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
-
-def starts_with_consonant? s
-  # YOUR CODE HERE
+#================
+def starts_with_consonant?(s)
+  /^[b-df-hj-np-tv-z]/i.match(s) != nil
 end
-
-def binary_multiple_of_4? s
-  # YOUR CODE HERE
+#================
+def binary_multiple_of_4?(s)
+  return true if s == "0"
+	/^[10]*00$/.match(s) != nil
 end
-
+#================
+#================
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  
+	def initialize(isbn, price)
+		raise ArgumentError, 
+			"Illegal argument" if isbn.empty?  or price <= 0
+		@isbn = isbn
+		@price = price
+	end
+	
+	attr_accessor :isbn
+	attr_accessor :price 
+	
+	def price_as_string
+		sprintf("$%2.2f", @price)
+	end
+	
 end
+#================
